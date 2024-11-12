488f955b3d0ca69cbc2b412546525f85c3b8e1f6
zobweyt <zobweyt@gmail.com>
2024-11-12 22:53:17 +0300

    feat: make `get_session` function public

--- src/db/deps.py
+++ src/db/deps.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Session as __Session
 from src.db import ENGINE
 
 
-def __get_session() -> Generator[__Session, None, None]:
+def get_session() -> Generator[__Session, None, None]:
     session = __Session(ENGINE)
     try:
         yield session
@@ -17,4 +17,4 @@ def __get_session() -> Generator[__Session, None, None]:
         session.close()
 
 
-Session = Annotated[__Session, Depends(__get_session)]
+Session = Annotated[__Session, Depends(get_session)]
