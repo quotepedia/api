0b27863518c85525e01fe0c6fd8ea5cedbb1423c
zobweyt <zobweyt@gmail.com>
2024-11-12 23:11:27 +0300

    style: organize imports

--- src/api/v1/auth/__init__.py
+++ src/api/v1/auth/__init__.py
@@ -1,6 +1,6 @@
+from src.api.v1.auth.deps import OAuth2BearerDepends, OAuth2PasswordRequestFormDepends, OptionalOAuth2BearerDepends
 from src.api.v1.auth.routes import router
-from src.api.v1.auth.deps import OAuth2BearerDepends, OptionalOAuth2BearerDepends, OAuth2PasswordRequestFormDepends
-from src.api.v1.auth.schemas import AccessTokenResponse, JWT
+from src.api.v1.auth.schemas import JWT, AccessTokenResponse
 
 __all__ = [
     "router",
