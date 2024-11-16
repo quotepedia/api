2ee4ad019d01b6f6e01252b7b059d9cbecbec940
zobweyt <zobweyt@gmail.com>
2024-11-16 12:24:30 +0300

    perf: use frozen slots dataclass for search params

--- src/api/params.py
+++ src/api/params.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from src.config import settings
 
 
-@dataclass
+@dataclass(frozen=True, slots=True)
 class SearchParams:
     q: str | None = None
     offset: int = 0
